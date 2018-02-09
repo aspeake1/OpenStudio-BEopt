95cfef48522f262fa92e35e6365ffa9a37eafffd
Scott Horowitz <scott.horowitz@nrel.gov>
2018-02-09 08:06:28 -0700

    Attempt 2.

--- test/bin/docker-run.sh
+++ test/bin/docker-run.sh
@@ -2,7 +2,7 @@
 
 # Temporary fix; should be done by OpenStudio 2.0.6 installer
 apt-get update
-apt-get install -y libglu1-mesa libjpeg8 libfreetype6 libdbus-glib-1-2 libfontconfig1 libsm6 libxi6 autotools-dev
+apt-get install -y libglu1-mesa libjpeg8 libfreetype6 libdbus-glib-1-2 libfontconfig1 libsm6 libxi6 autoconf
 
 export CI=true
 export CIRCLECI=true
