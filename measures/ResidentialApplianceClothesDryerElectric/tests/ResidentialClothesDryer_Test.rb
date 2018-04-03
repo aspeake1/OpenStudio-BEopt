a9905bffaf493aa1350913070162e109da66e1bb
jmaguire1 <Jeff.Maguire@nrel.gov>
2018-04-03 08:52:58 -0600

    Uncommenting "show_output" in the failing tests so I can figure out why unit tests are failing on the CircleCI machine but passing on mine

--- measures/ResidentialApplianceClothesDryerElectric/tests/ResidentialClothesDryer_Test.rb
+++ measures/ResidentialApplianceClothesDryerElectric/tests/ResidentialClothesDryer_Test.rb
@@ -248,7 +248,7 @@ class ResidentialClothesDryerTest < MiniTest::Test
     result = runner.result
 
     # show the output
-    #show_output(result)
+    show_output(result)
 
     # assert that it didn't run
     assert_equal("Fail", result.value.valueName)
@@ -292,7 +292,7 @@ class ResidentialClothesDryerTest < MiniTest::Test
     result = runner.result
 
     # show the output
-    #show_output(result)
+    show_output(result)
 
     # assert that it ran correctly
     assert_equal("Success", result.value.valueName)
