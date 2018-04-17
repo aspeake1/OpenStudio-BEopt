8b60181e88bc0d6460873eeb74e64d7708aa7611
Joe Robertson <joseph.robertson@nrel.gov>
2018-04-17 09:25:00 -0600

    Comment.

--- measures/UtilityBillCalculations/tests/utility_bill_calculations_test.rb
+++ measures/UtilityBillCalculations/tests/utility_bill_calculations_test.rb
@@ -595,7 +595,7 @@ class UtilityBillCalculationsTest < MiniTest::Test
     measure.calculate_utility_bills(runner, timeseries, weather_file_state, marginal_rates, fixed_rates, args_hash["pv_compensation_type"], args_hash["pv_annual_excess_sellback_rate_type"], args_hash["pv_sellback_rate"], args_hash["pv_tariff_rate"], args_hash["electric_bill_type"], tariffs)
 
     result = runner.result
-    show_output(result)
+    # show_output(result)
     
     # assert that it ran correctly
     assert_equal("Success", result.value.valueName)
