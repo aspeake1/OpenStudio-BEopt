853663e7e76fcc8c7417804631334b3591257e86
Scott Horowitz <scott.horowitz@nrel.gov>
2018-05-07 13:18:01 -0600

    Revert from AlwaysOff shading schedule for skylights. Add unit tests to ensure shading controls are assigned to subsurfaces. A few other minor changes.

--- measures/ResidentialConstructionsFinishedRoof/resources/constructions.rb
+++ measures/ResidentialConstructionsFinishedRoof/resources/constructions.rb
@@ -1743,22 +1743,12 @@ class SubsurfaceConstructions
             sm.setRightSideOpeningMultiplier(0)
             sm.setAirflowPermeability(0)
 
-            if type == "Window"
-              # WindowShadingControl
-              sc = OpenStudio::Model::ShadingControl.new(sm)
-              sc.setName("#{type}ShadingControl")
-              sc.setShadingType("InteriorShade")
-              sc.setShadingControlType("OnIfScheduleAllows")
-              sc.setSchedule(sch.schedule)
-            elsif type == "Skylight"
-              # SkylightShadingControl
-              sc = OpenStudio::Model::ShadingControl.new(sm)
-              sc.setName("#{type}ShadingControl")
-              sc.setShadingType("InteriorShade")
-              sc.setShadingControlType("AlwaysOff")
-              # sc.setSchedule(model.alwaysOffDiscreteSchedule)
-            end
-            
+            # ShadingControl
+            sc = OpenStudio::Model::ShadingControl.new(sm)
+            sc.setName("#{type}ShadingControl")
+            sc.setShadingType("InteriorShade")
+            sc.setShadingControlType("OnIfScheduleAllows")
+            sc.setSchedule(sch.schedule)
         end
 
         # Define materials
