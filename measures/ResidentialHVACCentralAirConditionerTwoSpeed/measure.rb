b256ef0dc8a0a5e07fabdc1430aaf7b0a527ebeb
Joe Robertson <joseph.robertson@nrel.gov>
2018-07-24 15:52:30 -0600

    Comparing models with multiple multi speed central acs and ashps.

--- measures/ResidentialHVACCentralAirConditionerTwoSpeed/measure.rb
+++ measures/ResidentialHVACCentralAirConditionerTwoSpeed/measure.rb
@@ -223,7 +223,7 @@ class ProcessTwoSpeedCentralAirConditioner < OpenStudio::Measure::ModelMeasure
       thermal_zones = Geometry.get_thermal_zones_from_spaces(unit.spaces)
       HVAC.get_control_and_slave_zones(thermal_zones).each do |control_zone, slave_zones|
         ([control_zone] + slave_zones).each do |zone|
-          HVAC.remove_hvac_equipment(model, runner, zone, unit, Constants.ObjectNameCentralAirConditioner)
+          #HVAC.remove_hvac_equipment(model, runner, zone, unit, Constants.ObjectNameCentralAirConditioner)
         end
       end
       
